294cba5175b653c0b4ecaff244af449de85fbb36
zaryab489 <ranazaryab455@gmail.com>
2025-08-25 00:25:33 +0500

    Initial Commit

---
+++ SwiftUI-Todo-ListApp/SwiftUI_Todo_ListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Todo_ListAppApp.swift
+//  SwiftUI-Todo-ListApp
+//
+//  Created by Macbook Pro on 25/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Todo_ListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftUI-Todo-ListAppTests/SwiftUI_Todo_ListAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Todo_ListAppTests.swift
+//  SwiftUI-Todo-ListAppTests
+//
+//  Created by Macbook Pro on 25/08/2025.
+//
+
+import Testing
+@testable import SwiftUI_Todo_ListApp
+
+struct SwiftUI_Todo_ListAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
