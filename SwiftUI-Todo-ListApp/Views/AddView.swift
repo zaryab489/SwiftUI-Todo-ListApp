330ea075925ad012d43d88eeb27c084fe96b1058
zaryab489 <ranazaryab455@gmail.com>
2025-08-25 01:09:34 +0500

    Add view

--- SwiftUI-Todo-ListApp/Views/AddView.swift
+++ SwiftUI-Todo-ListApp/Views/AddView.swift
@@ -6,13 +6,37 @@
 //
 
 import SwiftUI
-
 struct AddView: View {
+    @State var textFieldText: String = ""
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView {
+            VStack {
+                TextField("Type something here...", text: $textFieldText)
+                    .padding(.horizontal)
+                    .frame(height:55)
+                    .background(Color(.lightGray))
+                    .cornerRadius(10)
+                Button(action: {
+                    
+                }, label: {
+                    Text("Save".uppercased())
+                        .foregroundColor(.white)
+                        .font(.headline)
+                        .frame(height:55)
+                        .frame(maxWidth:.infinity)
+                        .background(Color.accentColor)
+                        .cornerRadius(10)
+                })
+            }
+            .padding(14)
+        }
+        .navigationTitle("Add an Item 🖋️")
     }
 }
 
 #Preview {
-    AddView()
+    NavigationView {
+        AddView()
+    }
+   
 }
