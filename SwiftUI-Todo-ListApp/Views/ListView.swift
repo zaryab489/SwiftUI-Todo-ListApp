8aaa802a42b297e358280fcc0d076284ad016e1b
zaryab489 <ranazaryab455@gmail.com>
2025-08-25 01:12:09 +0500

    listView

--- SwiftUI-Todo-ListApp/Views/ListView.swift
+++ SwiftUI-Todo-ListApp/Views/ListView.swift
@@ -8,11 +8,28 @@
 import SwiftUI
 
 struct ListView: View {
+    @State var items: [String] = [
+        "This is the first item!",
+        "This is the second",
+        "Third"
+    ]
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+        List {
+            ForEach(items, id: \.self) { item in
+                ListRowView(title: item)
+            }
+        }
+        .listStyle(PlainListStyle())
+        .navigationTitle("Todo List 📝")
+        .navigationBarItems(leading: EditButton(), trailing:
+        NavigationLink("Add", destination: AddView())
+        )
+        }
 }
 
 #Preview {
-    ListView()
+    NavigationView {
+        ListView()
+    }
+    
 }
