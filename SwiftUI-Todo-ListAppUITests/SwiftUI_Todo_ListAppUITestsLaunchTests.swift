648cf00c5d21280543e14ddc8f238da1044a952f
zaryab489 <ranazaryab455@gmail.com>
2025-08-25 00:25:33 +0500

    Initial Commit

---
+++ SwiftUI-Todo-ListAppUITests/SwiftUI_Todo_ListAppUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  SwiftUI_Todo_ListAppUITestsLaunchTests.swift
+//  SwiftUI-Todo-ListAppUITests
+//
+//  Created by Macbook Pro on 25/08/2025.
+//
+
+import XCTest
+
+final class SwiftUI_Todo_ListAppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
