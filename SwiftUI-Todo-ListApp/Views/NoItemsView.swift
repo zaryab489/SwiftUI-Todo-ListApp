92a826cbf13a797bff070c0bc1250fbc4e41f94b
zaryab489 <ranazaryab455@gmail.com>
2025-09-02 22:40:38 +0500

    Update no items list

--- SwiftUI-Todo-ListApp/Views/NoItemsView.swift
+++ SwiftUI-Todo-ListApp/Views/NoItemsView.swift
@@ -9,10 +9,33 @@ import SwiftUI
 
 struct NoItemsView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView {
+            VStack (spacing: 10) {
+                Text("There are no items!")
+                    .font(.title)
+                    .fontWeight(.semibold)
+                Text("Are you a productive person? I think you should click the add button and add a bunch of items to your todo list!")
+                NavigationLink(destination: AddView(),label: {
+                    Text("Add Something 🥳")
+                        .foregroundColor(.white)
+                        .font(.headline)
+                        .frame(height:55)
+                        .frame(maxWidth: .infinity)
+                        .background(Color.accentColor)
+                        .cornerRadius(10)
+                })
+                    
+            }
+            .multilineTextAlignment(.center)
+            .padding(40)
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
     }
 }
 
 #Preview {
-    NoItemsView()
+    NavigationView {
+        NoItemsView()
+            .navigationTitle("Title")
+    }
 }
