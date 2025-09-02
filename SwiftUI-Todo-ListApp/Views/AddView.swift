75d3626c6c23ff25b84f82cb305d9a2bc88d672d
zaryab489 <ranazaryab455@gmail.com>
2025-09-02 19:20:37 +0500

    update AddView with the alert

--- SwiftUI-Todo-ListApp/Views/AddView.swift
+++ SwiftUI-Todo-ListApp/Views/AddView.swift
@@ -11,6 +11,10 @@ struct AddView: View {
     @Environment(\.presentationMode) var presentationMode
     @EnvironmentObject var listViewModel: ListViewModel
     @State var textFieldText: String = ""
+    
+    @State var alertTitle: String = ""
+    @State var showAlert : Bool = false
+    
     var body: some View {
         ScrollView {
             VStack {
@@ -32,10 +36,28 @@ struct AddView: View {
             .padding(14)
         }
         .navigationTitle("Add an Item 🖋️")
+        .alert(isPresented: $showAlert, content: getAlert)
     }
     func saveButtonPressed() {
-        listViewModel.addItem(title: textFieldText)
-        presentationMode.wrappedValue.dismiss()
+        if textIsAppropriate() {
+            
+            listViewModel.addItem(title: textFieldText)
+            presentationMode.wrappedValue.dismiss()
+        }
+    
+    }
+    
+    func textIsAppropriate() -> Bool {
+        if textFieldText.count < 3 {
+            alertTitle = "Your new todo item must be at least 3 characters long!!!😨😰😱"
+            showAlert.toggle()
+            return false
+        }
+        return true
+    }
+    func getAlert() -> Alert {
+        return Alert(title: Text(alertTitle))
+        
     }
 }
 
