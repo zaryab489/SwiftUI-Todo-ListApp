2909cfff48f9424d302ade0031f490e553643470
zaryab489 <ranazaryab455@gmail.com>
2025-09-02 19:59:32 +0500

    Update itemModel

--- SwiftUI-Todo-ListApp/Models/itemModel.swift
+++ SwiftUI-Todo-ListApp/Models/itemModel.swift
@@ -7,10 +7,21 @@
 
 import Foundation
 
+// immutable Struct
+
 struct ItemModel: Identifiable {
     
-    let id: String = UUID().uuidString
+    let id: String
     let title: String
     let isCompleted: Bool
     
+    init(id: String = UUID().uuidString, title: String, isCompleted: Bool) {
+        self.id = id
+        self.title = title
+        self.isCompleted = isCompleted
+        
+    }
+    func updateCompletion()-> ItemModel {
+        return ItemModel(id: id, title: title, isCompleted: !isCompleted)
+    }
 }
