b4905e73c7977b83557a04c311457369a1b1598c
zaryab489 <ranazaryab455@gmail.com>
2025-09-02 22:40:00 +0500

    Add noitemslist file

---
+++ SwiftUI-Todo-ListApp/Views/NoItemsView.swift
@@ -0,0 +1,18 @@
+//
+//  NoItemsView.swift
+//  SwiftUI-Todo-ListApp
+//
+//  Created by Macbook Pro on 02/09/2025.
+//
+
+import SwiftUI
+
+struct NoItemsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NoItemsView()
+}
