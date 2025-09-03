37fe1c752b7c417e3b9e1cc2ed9df04306e1ecc6
zaryab489 <ranazaryab455@gmail.com>
2025-09-03 20:09:58 +0500

    Update name

--- SwiftUI-Todo-ListApp/Views/AddView.swift
+++ SwiftUI-Todo-ListApp/Views/AddView.swift
@@ -21,7 +21,7 @@ struct AddView: View {
                 TextField("Type something here...", text: $textFieldText)
                     .padding(.horizontal)
                     .frame(height:55)
-                    .background(Color(.lightGray))
+                    .background(Color(UIColor.secondarySystemBackground))
                     .cornerRadius(10)
                 Button(action: saveButtonPressed, label: {
                     Text("Save".uppercased())
